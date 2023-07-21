beeefda7f2be7d3f4125635900cb586d678a8b82
bea-bp <129992845+bea-bp@users.noreply.github.com>
2023-07-21 14:30:22 +0000

    familyApi

--- src/app.py
+++ src/app.py
@@ -8,37 +8,38 @@ from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 #from models import Person
 
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
-jackson_family = FamilyStructure("Jackson")
-
-# Handle/serialize errors like a JSON object
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
-
-# generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
-    return generate_sitemap(app)
+family = FamilyStructure("Jackson")
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
+    members = family.get_all_members()
+    return jsonify(members), 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_one_member(member_id):
+    member = family.get_member(member_id)
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+    return jsonify(member), 200
 
+@app.route('/member', methods=['POST'])
+def add_member():
+    member_info = request.get_json()
+    if not member_info:
+        return jsonify({"error": "Bad request"}), 400
+    member = family.add_member(member_info)
+    return jsonify(member), 200
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = family.delete_member(member_id)
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+    return jsonify({"done": True}), 200
 
-# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
